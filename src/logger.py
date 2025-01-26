7f3956ac86a75d5f959ea39cec990fd61cd91fee
Ksrvinay <viinnayyyy@gmail.com>
2025-01-26 17:14:41 +0530

    Updated logging setup with directory creation and file handling

--- src/logger.py
+++ src/logger.py
@@ -2,17 +2,24 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+# Create a log file name based on the current datetime
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+# Define the logs directory path
+logs_path = os.path.join(os.getcwd(), "logs")
 
+# Create the logs directory if it doesn't exist
+os.makedirs(logs_path, exist_ok=True)
+
+# Define the full log file path by joining the logs directory and the log file name
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# Set up logging configuration
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
 
-if __name__=="__main__":
+if __name__ == "__main__":
     logging.info("Logging has started")
